bff4466a8a09e61c1443b9dc61c911322da7c4b5
ryosuke-horie <r_horie@sincereed.com>
2025-03-26 00:03:03 +0900

    fix: Headerのみダークテーマ適用ができないため修正

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -13,7 +13,7 @@ export function Header() {
 	};
 
 	return (
-		<header className="bg-white shadow-sm">
+		<header className="shadow-sm">
 			<div className="container mx-auto px-4">
 				<div className="flex justify-between items-center h-16">
 					<div className="flex-shrink-0">
