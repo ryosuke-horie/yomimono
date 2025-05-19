d8b4a257dd79ed2e7a43e80b8bf5272b5b788443
ryosuke-horie <r_horie@sincereed.com>
2025-05-19 09:27:55 +0900

    feat: knip依存関係の削除

--- api/knip.config.ts
+++
@@ -1,7 +0,0 @@
-import type { KnipConfig } from "knip";
-
-const config: KnipConfig = {
-	ignore: ["worker-configuration.d.ts", "scripts/run-batch-manually.ts"],
-};
-
-export default config;
--- extension/knip.config.ts
+++
@@ -1,7 +0,0 @@
-import type { KnipConfig } from "knip";
-
-const config: KnipConfig = {
-	ignore: ["popup/popup.js"],
-};
-
-export default config;
